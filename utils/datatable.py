c10b0e6da59357c638bafd69e78e01eb61d976ad
Younglion6 <jonathan_drakes@hotmail.com>
2023-12-09 10:56:49 -0400

    database.py creation

--- utils/datatable.py
+++ utils/datatable.py
@@ -1,57 +1,61 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
 
-from collections import OrderedDict
 from pymongo import MongoClient
+from collections import OrderedDict
 
-class AdminWindow(BoxLayout):
+Builder.load_string('''
+<DataTable>:
+    id: main_win
+    RecycleView:
+        viewclass: 'CustLabel'
+        id: table_floor
+        RecycleGridLayout:
+            id: table_floor_layout
+            cols: 5
+            default_size: (None,250)
+            default_size_hint: (1,None)
+            size_hint_y: None
+            height: self.minimum_height
+            spacing: 5
+<CustLabel@Label>:
+    bcolor: (1,1,1,1)
+    canvas.before:
+        Color:
+            rgba: root.bcolor
+        Rectangle:
+            size: self.size
+            pos: self.pos
+''')
+
+class DataTable(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
-    #     stb = {
+        
+        products = self.get_products()
+        
+         #     stb = {
     #     'TH0':{0:'St0',1:'Sample1',2:'Sample2',3:'Sample4'},
     #     'TH1':{0:'Stm0',1:'Sample1',2:'Sample2',3:'Sample4'},
     #     'TH2':{0:'Stmp0',1:'Sampled1.0',2:'Sampled2.0',3:'Sampled4.0'},
     #     'TH3':{0:'Stmpl0',1:'Sample1',2:'Sample2',3:'Sample4'},
     #     'TH4':{0:'Stmple0',1:'Sample1',2:'Sample2',3:'Sample4'},
-        print(self.get_products())
-    # }
-    def get_users(self):
-        client = MongoClient()
-        db = client.silverpos
-        users = db.users
-        _users = OrderedDict(
-            first_names = {},
-            last_names = {},
-            user_names = {},
-            passwords = {},
-            designations = {}
-        )
-        first_names = []
-        last_names = []
-        user_names = []
-        passwords = []
-        designations = []
-        for user in users.find():
-            first_names.append(user['first_name'])
-            last_names.append(user['last_name'])
-            user_names.append(user['user_name'])
-            passwords.append(user['password'])
-            designations.append(user['designation'])
-        # print(designations)
-        users_length = len(first_names)
-        idx = 0
-        while idx < users_length:
-            _users['first_names'][idx] = first_names[idx]
-            _users['last_names'][idx] = last_names[idx]
-            _users['user_names'][idx] = user_names[idx]
-            _users['passwords'][idx] = passwords[idx]
-            _users['designations'][idx] = designations[idx]
-
-            idx += 1
+    
+        col_titles = [k for k in products.keys()]
+        rows_len = len(products[col_titles[0]])
+        self.columns = len(col_titles)
+        table_data = []
+        for t in col_titles:
+            table_data.append({'text': str(t),'size_hint_y': None, 'height': 50, 'bcolor':(.06,.45,.45,1)})
+            
+            
+        for r in range(rows_len):
+            for t in col_titles:
+                table_data.append({'text':str(products[t][r]),'size_hint_y': None, 'height': 30, 'bcolor':(.06, .25, .25, 1)})
+        self.ids.table_floor_layout.cols = self.columns
+        self.ids.table_floor.data = table_data
         
-        return _users
-
     def get_products(self):
         client = MongoClient()
         db = client.silverpos
@@ -98,13 +102,11 @@ class AdminWindow(BoxLayout):
         
         return _stocks
         
-
-
-
-class AdminApp(App):
+        
+class DataTableApp(App):
     def build(self):
-
-        return AdminWindow()
-
-if __name__=='__main__':
-    AdminApp().run()
+        
+        return DataTable()
+    
+if __name__ == '__main__':
+    DataTableApp().run()
